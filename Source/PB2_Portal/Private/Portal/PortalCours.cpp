b320878340bccff0995c4f7600cf7544d91627bb
MisterHF <h.florianpro@gmail.com>
2025-03-10 17:56:56 +0100

    test

---
+++ Source/PB2_Portal/Private/Portal/PortalCours.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Portal/PortalCours.h"
+
+// Sets default values
+APortalCours::APortalCours()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APortalCours::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APortalCours::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+
---
+++ Source/PB2_Portal/Public/Portal/MyLocalPlayerSubsystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/LocalPlayerSubsystem.h"
+#include "MyLocalPlayerSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PB2_PORTAL_API UMyLocalPlayerSubsystem : public ULocalPlayerSubsystem
+{
+	GENERATED_BODY()
+	
+};
