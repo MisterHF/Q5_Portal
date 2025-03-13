61f2ff1288a8c721c388835bfc253136861ac3e5
MisterHF <h.florianpro@gmail.com>
2025-03-13 14:56:28 +0100

    Comment Begin Play and Tick function in .h

--- Source/PB2_Portal/Public/APortal.h
+++ Source/PB2_Portal/Public/APortal.h
@@ -15,13 +15,13 @@ public:
 	// Sets default values for this actor's properties
 	AAPortal();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+//protected:
+//	// Called when the game starts or when spawned
+//	virtual void BeginPlay() override;
+//
+//public:	
+//	// Called every frame
+//	virtual void Tick(float DeltaTime) override;
 
 	//Variable
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Portal")
